50967d764893c2ba6091bd33f0709286e4411338
jasonwang <wangstevens0412@gmail.com>
2015-11-30 14:44:01 +0800

    add codes and readme

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # RotateViewPage
-ViewPager在切换的时候，页面会有一定角度的切换
+ViewPager在切换的时候，页面会有一定角度的切换。
 #使用方法
+1.ViewPager本身的设置：
+
+```
+<android.support.v4.view.ViewPager
+    android:id="@+id/pager"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:clipToPadding="false"
+    android:paddingLeft="100px"
+    android:paddingRight="100px"/>
+
+```
+为它设置左右padding，然后clipToPadding = false，这样就可以看见和边上page交互转动的过程了。
+
+2.将page之间的margin设置为负值，这样就可以在当前页面看见左右的page了。然后就是将ZoomOutPageTransformer设置为ViewPager的切换动画：
+
+```
+mPager.setPageMargin(-50);
+mPager.setPageTransformer(true,new ZoomOutPageTransformer(mPager));
+```
--- sample/src/main/java/com/stevenswang/app/testrotateviewpage/MainActivity.java
+++ sample/src/main/java/com/stevenswang/app/testrotateviewpage/MainActivity.java
@@ -1,52 +1,49 @@
 package com.stevenswang.app.testrotateviewpage;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v13.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import com.jason.component.rotateviewpage.ZoomOutPageTransformer;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int NUM_PAGES = 5;
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_screen_slide);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setBackgroundColor(Color.BLUE);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+        mPager.setPageMargin(-50);
+        mPager.setPageTransformer(true,new ZoomOutPageTransformer(mPager));
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public Fragment getItem(int position) {
+            return ScreenSlidePageFragment.create(position, getCount());
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+
+        @Override
+        public float getPageWidth(int position) {
+            return 1.0f;
+        }
     }
 }
